fa1219368acf2ae3803487a65dd756bcc0792693
Hemanth <hemanthreddy056@gmail.com>
2025-05-28 12:00:48 +0530

    added the grocery DTO

--- Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+
+public struct GroceryCategoryResponseDTO: Codable {
+    public let id:UUID
+    public let title:String
+    public let colorCode:String
+    
+    public init(id: UUID = UUID(), title: String, colorCode: String) {
+        self.id = id
+        self.title = title
+        self.colorCode = colorCode
+    }
+    
+}
