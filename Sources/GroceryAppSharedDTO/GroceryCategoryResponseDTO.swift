d245edfa3cd94fa3b5630a32759f0d97d5a704f7
Hemanth <hemanthreddy056@gmail.com>
2025-06-03 11:55:25 +0530

    changing GroceryCategoryResponseDTO to include groceryItems in the Model

--- Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -12,11 +12,13 @@ public struct GroceryCategoryResponseDTO: Codable {
     public let id:UUID
     public let title:String
     public let colorCode:String
+    public let items:[GroceryItemResponsetDTO]
     
-    public init(id: UUID = UUID(), title: String, colorCode: String) {
+    public init(id: UUID = UUID(), title: String, colorCode: String,items:[GroceryItemResponsetDTO] = []) {
         self.id = id
         self.title = title
         self.colorCode = colorCode
+        self.items = items
     }
     
 }
